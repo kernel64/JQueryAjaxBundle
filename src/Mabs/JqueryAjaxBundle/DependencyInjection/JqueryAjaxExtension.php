bb183f56e7f74162ba78802147669b65165fe29e
unknown <mabslimane@Talan180.dom.tti>
2013-10-25 10:08:39 +0100

    autoload service.yml

---
+++ src/Mabs/JqueryAjaxBundle/DependencyInjection/JqueryAjaxExtension.php
@@ -0,0 +1,16 @@
+<?php
+namespace Mabs\JQueryAjaxBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class JqueryAjaxExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}

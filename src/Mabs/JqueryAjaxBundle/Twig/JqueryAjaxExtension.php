ea19d3f4e9e6db978394a1f50b6ee5c568714705
Mohamed Aymen <aymen.kernel@gmail.com>
2013-11-15 02:30:07 -0800

    Update JqueryAjaxExtension.php
    add Confirmation request to generated link

--- src/Mabs/JqueryAjaxBundle/Twig/JqueryAjaxExtension.php
+++ src/Mabs/JqueryAjaxBundle/Twig/JqueryAjaxExtension.php
@@ -69,10 +69,19 @@ class JqueryAjaxExtension extends \Twig_Extension
         
         return function ($options) use($jsRequest)
         {
+            $confirm = '';
+            if (isset($options['confirm']) && $options['confirm'] == true) {
+                
+                $msg = "Are you sure you want to perform this action?";
+                if(isset($options['confirm_msg'])) {
+                    $msg = htmlentities(str_replace("'", '"', $options['confirm_msg']), ENT_QUOTES);                  
+                }
+            	$confirm .= "if(confirm('".$msg."'))" ;
+            }
             $html = '<a class="' . (isset($options['class']) ? $options['class'] : "") . '"
     		 		 id="' . (isset($options['id']) ? $options['id'] : "") . '"
     		 		 href="' . $options['url'] . '"
-    				 onclick="' . call_user_func($jsRequest, $options) . 'return false;">';
+    				 onclick="' .$confirm. call_user_func($jsRequest, $options) . 'return false;">';
             $html .= $options['text'];
             $html .= '</a>';
             
